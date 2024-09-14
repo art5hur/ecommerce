88d99ee0ff35ddaf956988acde8a8f30812cd445
Laboratório FIAP <labsfiap@fiap.com.br>
2024-09-13 22:41:41 -0300

    Dto parte 1

--- src/main/java/br/com/fiap/ecommerce/controller/ProdutoController.java
+++ src/main/java/br/com/fiap/ecommerce/controller/ProdutoController.java
@@ -1,17 +1,23 @@
 package br.com.fiap.ecommerce.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.fiap.ecommerce.dtos.ProdutoRequestCreateDto;
 import br.com.fiap.ecommerce.dtos.ProdutoResponseDto;
+import br.com.fiap.ecommerce.dtos.ProdutoResponseUpdateDto;
 import br.com.fiap.ecommerce.model.Produto;
 import br.com.fiap.ecommerce.service.ProdutoService;
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
 
 
 @RestController
@@ -23,14 +29,64 @@ public class ProdutoController {
     
     @GetMapping
     public List<ProdutoResponseDto> list (){
-        
         List<Produto> produtos = produtoService.list();
+        //Produto -> List<ProdutoResponseDto>
         return null;
     }
 
    @PostMapping
-    public ProdutoResponseDto create (ProdutoRequestCreateDto dto) {
+    public ProdutoResponseDto create (@RequestBody ProdutoRequestCreateDto dto) {
+        //ProdutoRequestCreateDto -> Produto
+        //Produto saved = produtoService.save();
+        //Produto -> List<ProdutoResponseDto>
         return null;
     }
 
+    //localhost:8080/produtos/5
+    /*  body/payload: {
+            "id": 5.
+            "nome": "Um nome novo"
+        }
+    */
+
+    @PutMapping("{id}")
+    public ProdutoResponseDto update (
+                        @PathVariable Long id, 
+                        @RequestBody  ProdutoResponseUpdateDto dto){
+        if(! produtoService.existsById(id)){
+            new RuntimeException("Id inexistente");
+        }
+
+        // ProdutoRequestUpdateDto -> Produto
+        // Produto saved = produtoService.save(produto);
+        // Produto -> ProdutoResponseDto
+        return null;
+    }
+
+    @DeleteMapping("{id}")
+    public void delete (@PathVariable Long id){
+        
+
+           if(! produtoService.existsById(id)){
+            new RuntimeException("Id inexistente");
+        }
+
+        produtoService.delete(id);
+    }
+
+    @GetMapping("{id}")
+    public ProdutoResponseDto findById(@PathVariable Long id){
+        Optional<Produto> opt = produtoService.findById(id);
+
+        if (opt.isPresent()){
+            Produto produto = opt.get();
+            return null;
+        }else{
+            new RuntimeException("Id inexistente");
+        }
+
+
+
+        return null;
+}
 }
--- src/main/java/br/com/fiap/ecommerce/service/ProdutoService.java
+++ src/main/java/br/com/fiap/ecommerce/service/ProdutoService.java
@@ -1,10 +1,13 @@
 package br.com.fiap.ecommerce.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
 
+import br.com.fiap.ecommerce.dtos.ProdutoResponseDto;
 import br.com.fiap.ecommerce.model.Produto;
 import br.com.fiap.ecommerce.repository.ProdutoRepository;
 
@@ -12,9 +15,27 @@ import br.com.fiap.ecommerce.repository.ProdutoRepository;
 public class ProdutoService{
 
     @Autowired
-private ProdutoRepository produtoRepository;
+    private ProdutoRepository produtoRepository;
 
     public List<Produto> list(){
         return produtoRepository.findAll();
     }
+
+    public Produto save (Produto produto){
+        return produtoRepository.save(produto);
+    }
+
+    public boolean existsById(Long id) {
+        return produtoRepository.existsById(id);
+    }
+
+    public void delete(Long id) {
+        produtoRepository.deleteById(id);
+    }
+
+    public Optional<Produto> findById(Long id) {
+        return produtoRepository.findById(id);
+    }
+
+    
 }
