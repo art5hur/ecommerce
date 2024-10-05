53dddac500201cf459212e9464addc71914956bd
Laboratório FIAP <labsfiap@fiap.com.br>
2024-10-04 22:24:35 -0300

    Implementando lombok para otimizar getter and setter - @Data

--- src/main/java/br/com/fiap/ecommerce/model/Produto.java
+++ src/main/java/br/com/fiap/ecommerce/model/Produto.java
@@ -7,8 +7,10 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.Data;
 
 @Entity
+@Data
 public class Produto {
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
@@ -19,30 +21,6 @@ public class Produto {
 
 	@Column(precision = 16, scale = 2)
 	private BigDecimal valor;
-
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public String getNome() {
-		return nome;
-	}
-
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
-
-	public BigDecimal getValor() {
-		return valor;
-	}
-
-	public void setValor(BigDecimal valor) {
-		this.valor = valor;
-	}
 
 	
 
