e9266f36db31d5dc672d6f5992ccf834211f15f0
Laboratório FIAP <labsfiap@fiap.com.br>
2024-10-11 22:35:38 -0300

    Descrição com target

--- src/main/java/br/com/fiap/ecommerce/views/ProdutoFullView.java
+++ src/main/java/br/com/fiap/ecommerce/views/ProdutoFullView.java
@@ -1,6 +1,15 @@
 package br.com.fiap.ecommerce.views;
 
+
+import org.springframework.beans.factory.annotation.Value;
+
+
 public interface ProdutoFullView {
     String getNome();
-    Long getId();   
+    Long getId();
+    
+    @Value("#{target.id + '/' + target.nome}")
+    String getDescricao();
+
+    
 }
