18a2c6f0e72f2b35e4a1db82eff2a3d2c9a83ad4
Laboratório FIAP <labsfiap@fiap.com.br>
2024-10-11 22:39:47 -0300

    Valor multiplicado com target

--- src/main/java/br/com/fiap/ecommerce/views/ProdutoFullView.java
+++ src/main/java/br/com/fiap/ecommerce/views/ProdutoFullView.java
@@ -1,6 +1,8 @@
 package br.com.fiap.ecommerce.views;
 
 
+import java.math.BigDecimal;
+
 import org.springframework.beans.factory.annotation.Value;
 
 
@@ -11,5 +13,9 @@ public interface ProdutoFullView {
     @Value("#{target.id + '/' + target.nome}")
     String getDescricao();
 
+    BigDecimal getValor();
+
+    @Value("#{target.valor * 1.15}")
+    String getResultado();
     
 }
